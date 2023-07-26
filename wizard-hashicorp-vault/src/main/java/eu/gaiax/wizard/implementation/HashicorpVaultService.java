5ef3209c79317f631ca096c1af3b8335cbc6599e
Dilip Dhankecha <dilip.dhankecha@smartsensesolutions.com>
2023-07-26 12:56:22 +0530

    feat(VaultIntegration): Hashicorp Vault Support
    This commit contains below changes,
    1. Hashicorp vault integration which able to put and get the keys.
    2. Prepare seperate module for Hashicorp vault

---
+++ wizard-hashicorp-vault/src/main/java/eu/gaiax/wizard/implementation/HashicorpVaultService.java
@@ -0,0 +1,39 @@
+package eu.gaiax.wizard.implementation;
+
+import eu.gaiax.wizard.exception.VaultException;
+import eu.gaiax.wizard.model.VaultPathConfiguration;
+import eu.gaiax.wizard.vault.Vault;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+import org.springframework.vault.core.VaultKeyValueOperationsSupport;
+import org.springframework.vault.core.VaultTemplate;
+import org.springframework.vault.support.VaultResponse;
+
+import java.util.Map;
+
+@Service
+@RequiredArgsConstructor
+public class HashicorpVaultService implements Vault {
+    private final VaultPathConfiguration vaultPathConfiguration;
+    private final VaultTemplate template;
+
+    @Override
+    public Map<String, Object> put(String secretName, Map<String, Object> kv) {
+        try {
+            this.template.opsForKeyValue(this.vaultPathConfiguration.secretPath(), VaultKeyValueOperationsSupport.KeyValueBackend.KV_2).put(secretName, kv);
+            return kv;
+        } catch (Exception ex) {
+            throw new VaultException("HashicorpVaultService(put): Issue occur while put the secrets on hashicorp vault.", ex);
+        }
+    }
+
+    @Override
+    public Map<String, Object> get(String secretName) {
+        try {
+            VaultResponse response = this.template.opsForKeyValue(this.vaultPathConfiguration.secretPath(), VaultKeyValueOperationsSupport.KeyValueBackend.KV_2).get(secretName);
+            return response.getData();
+        } catch (Exception ex) {
+            throw new VaultException("HashicorpVaultService(get): Issue occur while read the secrets from hashicorp vault.", ex);
+        }
+    }
+}
