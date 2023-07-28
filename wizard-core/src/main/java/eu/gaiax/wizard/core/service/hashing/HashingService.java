dc1942674c6fd6312da0efcece273a721da1b618
Dilip Dhankecha <dilip.dhankecha@smartsensesolutions.com>
2023-07-28 17:04:32 +0530

    feat(Hash): Add HashingService
    This commit contains below changes,
    1. Hashing service which will convert the string to hash.
    2. Hashing service supports a SHA512 and SHA256 algorithm.

--- wizard-core/src/main/java/eu/gaiax/wizard/core/service/hashing/HashingService.java
+++ wizard-core/src/main/java/eu/gaiax/wizard/core/service/hashing/HashingService.java
@@ -2,14 +2,45 @@ package eu.gaiax.wizard.core.service.hashing;
 
 import lombok.AccessLevel;
 import lombok.NoArgsConstructor;
+import lombok.SneakyThrows;
+import org.jetbrains.annotations.NotNull;
 import org.springframework.stereotype.Component;
+
+import java.nio.charset.StandardCharsets;
+import java.security.MessageDigest;
+import java.util.stream.IntStream;
 
 @Component
 @NoArgsConstructor(access = AccessLevel.PRIVATE)
 public class HashingService {
 
+    public static final String SHA_256 = "SHA-256";
+    public static final String SHA_512 = "SHA-512";
+
     public static String generateSha256Hash(String content) {
-        //https://registry.gaia-x.eu/v1/api/termsAndConditions
-        return content;
+        return generateHash(SHA_256, content);
+    }
+
+    public static String generateSha512Hash(String content) {
+        return generateHash(SHA_512, content);
+    }
+
+    @SneakyThrows
+    private static String generateHash(String algorithm, String tncContent) {
+        MessageDigest digest = MessageDigest.getInstance(algorithm);
+        return prepareHash(tncContent, digest);
+    }
+
+    @NotNull
+    private static String prepareHash(String tncContent, MessageDigest digest) {
+        byte[] hash = digest.digest(tncContent.getBytes(StandardCharsets.UTF_8));
+        StringBuilder hexString = new StringBuilder();
+        IntStream.range(0, hash.length).mapToObj(i -> Integer.toHexString(0xff & hash[i])).forEach(hex -> {
+            if (hex.length() == 1) {
+                hexString.append('0');
+            }
+            hexString.append(hex);
+        });
+        return hexString.toString();
     }
 }
