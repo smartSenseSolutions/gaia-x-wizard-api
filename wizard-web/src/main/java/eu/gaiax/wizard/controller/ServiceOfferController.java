1bee0315e32db3e889529bce2c2c8ec52bf911b1
mittal <mittal.vaghela@smartsensesolutions.com>
2023-08-21 14:02:58 +0530

    add example on swagger doc

--- wizard-web/src/main/java/eu/gaiax/wizard/controller/ServiceOfferController.java
+++ wizard-web/src/main/java/eu/gaiax/wizard/controller/ServiceOfferController.java
@@ -11,6 +11,10 @@ import eu.gaiax.wizard.api.model.service_offer.ServiceOfferingLocationResponse;
 import eu.gaiax.wizard.core.service.service_offer.ResourceService;
 import eu.gaiax.wizard.core.service.service_offer.ServiceOfferService;
 import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.media.Content;
+import io.swagger.v3.oas.annotations.media.ExampleObject;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
@@ -34,6 +38,94 @@ public class ServiceOfferController extends BaseController {
     @Tag(name = "Service-Offering")
     @Operation(summary = "Create Service offering for enterprise, role = enterprise")
     @PostMapping(path = SERVICE_OFFER, consumes = APPLICATION_JSON_VALUE, produces = APPLICATION_JSON_VALUE)
+    @io.swagger.v3.oas.annotations.parameters.RequestBody(content = {
+            @Content(examples = {
+                    @ExampleObject(name = "Create Service offer", value = """
+                            {
+                              "name": "name of service",
+                              "verificationMethod": "did:web:exmaple.com",
+                              "email": "casio50@exmaple.com",
+                              "description": "test service data",
+                              "privateKey": "-----BEGIN PRIVATE KEY---  ----END PRIVATE KEY-----",
+                              "credentialSubject": {
+                                "gx:termsAndConditions": {
+                                  "gx:URL": "https://aws.amazon.com/service-terms/"
+                                },
+                                "gx:policy": {
+                                  "gx:location": [
+                                    "BE-BRU"
+                                  ]
+                                },
+                                "gx:dataAccountExport": {
+                                  "gx:requestType": "API",
+                                  "gx:accessType": "physical",
+                                  "gx:formatType": "pdf"
+                                },
+                                "gx:aggregationOf": [
+                                  {
+                                    "id": "https://exmaple.com/12081064-8878-477e-8092-564a240c69e2/resource_9a40cafd-43ed-41b0-a53e-4e2af164fde5.json"
+                                  }
+                                ],
+                                "gx:dependsOn": [
+                                  {
+                                    "id": "https://exmaple.com/12081064-8878-477e-8092-564a240c69e2/service_S7GZ.json"
+                                  }
+                                ],
+                                "gx:dataProtectionRegime": "GDPR2016",
+                                "type": "gx:ServiceOffering"
+                              }
+                            }
+                            """)
+            })
+    })
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200", description = "Service offering successfully.",
+                    content = {
+                            @Content(
+                                    examples = {
+                                            @ExampleObject(name = "Success Response", value = """
+                                                    {
+                                                      "status": 200,
+                                                      "payload": {
+                                                        "id": "785651fd-8292-4555-b05c-77c03cea2312",
+                                                        "email": "participant@example.in",
+                                                        "legalName": "Participant Example",
+                                                        "shortName": "ParticipantShortName",
+                                                        "entityType": {
+                                                          "id": "933fde99-d815-4d10-b414-ea2d88d10474",
+                                                          "type": "Social Enterprise",
+                                                          "active": true
+                                                        },
+                                                        "domain": "ParticipantShortName.smart-x.smartsenselabs.com",
+                                                        "participantType": "REGISTERED",
+                                                        "status": 0,
+                                                        "credential": "{\\"legalParticipant\\":{\\"credentialSubject\\":{\\"gx:legalName\\":\\"Participant Example\\",\\"gx:headquarterAddress\\":{\\"gx:countrySubdivisionCode\\":\\"BE-BRU\\"},\\"gx:legalAddress\\":{\\"gx:countrySubdivisionCode\\":\\"BE-BRU\\"}}},\\"legalRegistrationNumber\\":{\\"gx:leiCode\\":\\"9695007586XZAKPYJ703\\"}}",
+                                                        "ownDidSolution": false
+                                                      }
+                                                    }                                                                                                        
+                                                    """)
+                                    }
+                            )
+                    }),
+            @ApiResponse(responseCode = "400", description = "Invalid (Participant,Dependence On,Aggregation of )",
+                    content = {
+                            @Content(examples = {
+                                    @ExampleObject(name = "Validation failed", value = """
+                                            {
+                                              "message": "Validation has been failed.",
+                                              "status": 400,
+                                              "payload": {
+                                                "error": {
+                                                  "message": "Validation has been failed.",
+                                                  "status": 400,
+                                                  "timeStamp": 1692342059774
+                                                }
+                                              }
+                                            }
+                                            """)
+                            })
+                    }),
+    })
     public CommonResponse<ServiceOfferResponse> createServiceOffering(@Valid @RequestBody CreateServiceOfferingRequest request, Principal principal) throws IOException {
         //todo email changes remaining get from auth(String) this.requestForClaim("email", principal)
         return CommonResponse.of(this.serviceOfferService.createServiceOffering(request, request.getEmail()));
@@ -42,11 +134,169 @@ public class ServiceOfferController extends BaseController {
     @Tag(name = "Service-Offering")
     @Operation(summary = "Validate Service offering for enterprise, role = enterprise")
     @PostMapping(path = VALIDATE_SERVICE_OFFER, consumes = APPLICATION_JSON_VALUE, produces = APPLICATION_JSON_VALUE)
+    @io.swagger.v3.oas.annotations.parameters.RequestBody(content = {
+            @Content(examples = {
+                    @ExampleObject(name = "Create Service offer", value = """
+                            {
+                              "name": "name of service",
+                              "description": "test service data",
+                              "credentialSubject": {
+                                "gx:termsAndConditions": {
+                                  "gx:URL": "https://aws.amazon.com/service-terms/"
+                                },
+                                "gx:policy": {
+                                  "gx:location": [
+                                    "BE-BRU"
+                                  ]
+                                },
+                                "gx:dataAccountExport": {
+                                  "gx:requestType": "API",
+                                  "gx:accessType": "physical",
+                                  "gx:formatType": "pdf"
+                                },
+                                "gx:aggregationOf": [
+                                  {
+                                    "id": "https://exmaple.com/12081064-8878-477e-8092-564a240c69e2/resource_9a40cafd-43ed-41b0-a53e-4e2af164fde5.json"
+                                  }
+                                ],
+                                "gx:dependsOn": [
+                                  {
+                                    "id": "https://exmaple.com/12081064-8878-477e-8092-564a240c69e2/service_S7GZ.json"
+                                  }
+                                ],
+                                "gx:dataProtectionRegime": "GDPR2016",
+                                "type": "gx:ServiceOffering"
+                              }
+                            }
+                            """)
+            })
+    })
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200", description = "Service offering successfully.",
+                    content = {
+                            @Content(
+                                    examples = {
+                                            @ExampleObject(name = "Success Response", value = """
+                                                    {
+                                                      "status": 200,
+                                                      "payload": {
+                                                    }                                                                                                        
+                                                    """)
+                                    }
+                            )
+                    }),
+            @ApiResponse(responseCode = "400", description = "Invalid (Participant,Dependence On,Aggregation of )",
+                    content = {
+                            @Content(examples = {
+                                    @ExampleObject(name = "Validation failed", value = """
+                                            {
+                                              "message": "Validation has been failed.",
+                                              "status": 400,
+                                              "payload": {
+                                                "error": {
+                                                  "message": "Validation has been failed.",
+                                                  "status": 400,
+                                                  "timeStamp": 1692342059774
+                                                }
+                                              }
+                                            }
+                                            """)
+                            })
+                    }),
+    })
     public void validateServiceOfferRequest(@RequestBody CreateServiceOfferingRequest request, Principal principal) throws IOException {
         //todo email changes remaining get from auth(String) this.requestForClaim("email", principal)
         this.serviceOfferService.validateServiceOfferMainRequest(request);
     }
 
+    @io.swagger.v3.oas.annotations.parameters.RequestBody(content = {
+            @Content(examples = {
+                    @ExampleObject(name = "Create Service offer", value = """
+                            {
+                              "name": "name of service",
+                              "verificationMethod": "did:web:exmaple.com",
+                              "participantJsonUrl": "https://example.com/12081064-8878-477e-8092-564a240c69e2/participant.json",
+                              "description": "test service data",
+                              "privateKey": "-----BEGIN PRIVATE KEY---  ----END PRIVATE KEY-----",
+                              "credentialSubject": {
+                                "gx:termsAndConditions": {
+                                  "gx:URL": "https://aws.amazon.com/service-terms/"
+                                },
+                                "gx:policy": {
+                                  "gx:location": [
+                                    "BE-BRU"
+                                  ]
+                                },
+                                "gx:dataAccountExport": {
+                                  "gx:requestType": "API",
+                                  "gx:accessType": "physical",
+                                  "gx:formatType": "pdf"
+                                },
+                                "gx:aggregationOf": [
+                                  {
+                                    "id": "https://exmaple.com/12081064-8878-477e-8092-564a240c69e2/resource_9a40cafd-43ed-41b0-a53e-4e2af164fde5.json"
+                                  }
+                                ],
+                                "gx:dependsOn": [
+                                  {
+                                    "id": "https://exmaple.com/12081064-8878-477e-8092-564a240c69e2/service_S7GZ.json"
+                                  }
+                                ],
+                                "gx:dataProtectionRegime": "GDPR2016",
+                                "type": "gx:ServiceOffering"
+                              }
+                            }
+                            """)
+            })
+    })
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200", description = "Service offering successfully.",
+                    content = {
+                            @Content(
+                                    examples = {
+                                            @ExampleObject(name = "Success Response", value = """
+                                                    {
+                                                      "status": 200,
+                                                      "payload": {
+                                                        "id": "785651fd-8292-4555-b05c-77c03cea2312",
+                                                        "email": "participant@example.in",
+                                                        "legalName": "Participant Example",
+                                                        "shortName": "ParticipantShortName",
+                                                        "entityType": {
+                                                          "id": "933fde99-d815-4d10-b414-ea2d88d10474",
+                                                          "type": "Social Enterprise",
+                                                          "active": true
+                                                        },
+                                                        "domain": "ParticipantShortName.smart-x.smartsenselabs.com",
+                                                        "participantType": "REGISTERED",
+                                                        "status": 0,
+                                                        "credential": "{\\"legalParticipant\\":{\\"credentialSubject\\":{\\"gx:legalName\\":\\"Participant Example\\",\\"gx:headquarterAddress\\":{\\"gx:countrySubdivisionCode\\":\\"BE-BRU\\"},\\"gx:legalAddress\\":{\\"gx:countrySubdivisionCode\\":\\"BE-BRU\\"}}},\\"legalRegistrationNumber\\":{\\"gx:leiCode\\":\\"9695007586XZAKPYJ703\\"}}",
+                                                        "ownDidSolution": false
+                                                      }
+                                                    }                                                                                                        
+                                                    """)
+                                    }
+                            )
+                    }),
+            @ApiResponse(responseCode = "400", description = "Invalid (Participant,Dependence On,Aggregation of )",
+                    content = {
+                            @Content(examples = {
+                                    @ExampleObject(name = "Validation failed", value = """
+                                            {
+                                              "message": "Validation has been failed.",
+                                              "status": 400,
+                                              "payload": {
+                                                "error": {
+                                                  "message": "Validation has been failed.",
+                                                  "status": 400,
+                                                  "timeStamp": 1692342059774
+                                                }
+                                              }
+                                            }
+                                            """)
+                            })
+                    }),
+    })
     @Tag(name = "Service-Offering")
     @Operation(summary = "Create Service offering for enterprise, role = enterprise")
     @PostMapping(path = PUBLIC_SERVICE_OFFER, consumes = APPLICATION_JSON_VALUE, produces = APPLICATION_JSON_VALUE)
