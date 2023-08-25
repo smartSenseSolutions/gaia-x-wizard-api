12bd520f1821ec1cdf8f3f5a5eb5bc755f2ac616
neha.puraswani <neha.puraswani@smartsensesolutions.com>
2023-08-25 14:39:11 +0530

    fix(ParticipantService): Update datatype for deviceConfigured

--- wizard-api/src/main/java/eu/gaiax/wizard/api/model/CheckParticipantRegisteredResponse.java
+++ wizard-api/src/main/java/eu/gaiax/wizard/api/model/CheckParticipantRegisteredResponse.java
@@ -1,4 +1,4 @@
 package eu.gaiax.wizard.api.model;
 
-public record CheckParticipantRegisteredResponse(boolean userRegistered, boolean deviceConfigured) {
+public record CheckParticipantRegisteredResponse(boolean userRegistered, Boolean deviceConfigured) {
 }
