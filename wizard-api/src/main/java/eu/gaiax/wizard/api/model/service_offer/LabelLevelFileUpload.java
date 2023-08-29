b8bdb47f107c5388e189d93d66b1b4d096c32bac
neha.puraswani <neha.puraswani@smartsensesolutions.com>
2023-08-29 12:34:56 +0530

    fix(LabelLevel): Upload label level file fix

--- wizard-api/src/main/java/eu/gaiax/wizard/api/model/service_offer/LabelLevelFileUpload.java
+++ wizard-api/src/main/java/eu/gaiax/wizard/api/model/service_offer/LabelLevelFileUpload.java
@@ -3,8 +3,5 @@ package eu.gaiax.wizard.api.model.service_offer;
 import jakarta.validation.constraints.NotNull;
 import org.springframework.web.multipart.MultipartFile;
 
-public record LabelLevelFileUpload(@NotNull(message = "level is required") String level,
-                                   @NotNull(message = "type is required") String type,
-                                   @NotNull(message = "File can not be blank") MultipartFile file
-) {
+public record LabelLevelFileUpload(@NotNull(message = "File can not be blank") MultipartFile file) {
 }
