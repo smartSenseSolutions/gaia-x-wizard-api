36899080863b24ba11d3c26f5114113876cfd19b
neha.puraswani <neha.puraswani@smartsensesolutions.com>
2023-09-21 15:10:00 +0530

    fix(Resource): Resource subtype text fix

--- wizard-api/src/main/java/eu/gaiax/wizard/api/utils/StringPool.java
+++ wizard-api/src/main/java/eu/gaiax/wizard/api/utils/StringPool.java
@@ -72,7 +72,7 @@ public class StringPool {
     public static final String TARGET = "target";
     public static final String ASSIGNER = "assigner";
     public static final String ACTION = "action";
-    public static final String SUBTYPE = "subtype";
+    public static final String SUBTYPE = "subType";
     public static final String JSON_EXTENSION = ".json";
     public static final String AGGREGATION_OF = "gx:aggregationOf";
     public static final String DEPENDS_ON = "gx:dependsOn";
