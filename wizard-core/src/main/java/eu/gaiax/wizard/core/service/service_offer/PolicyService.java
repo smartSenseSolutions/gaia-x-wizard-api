d4f4c1170ce83e63fcf2f4f2542437df90d4acf2
neha.puraswani <neha.puraswani@smartsensesolutions.com>
2023-10-09 17:28:02 +0530

    test(ServiceOffer and Policy): Add unit tests

--- wizard-core/src/main/java/eu/gaiax/wizard/core/service/service_offer/PolicyService.java
+++ wizard-core/src/main/java/eu/gaiax/wizard/core/service/service_offer/PolicyService.java
@@ -88,7 +88,7 @@ public class PolicyService {
     }
 
     public String[] getLocationByServiceOfferingId(String serviceOfferingId) {
-        JsonNode serviceOffer = this.getServiceOffering(serviceOfferingId);
+        JsonNode serviceOffer = this.getServiceOffering(serviceOfferingId, "invalid.service.offer.url");
         JsonNode policyArray = this.getPolicyArrayFromServiceOffer(serviceOffer);
 
         if (policyArray != null && policyArray.has(0)) {
@@ -125,17 +125,10 @@ public class PolicyService {
     }
 
     @SneakyThrows
-    private JsonNode getCatalogueDescription(String catalogueUrl) {
+    protected JsonNode getServiceOffering(String catalogueUrl, String errorMessage) {
         String catalogue = InvokeService.executeRequest(catalogueUrl, HttpMethod.GET);
-        Validate.isNull(catalogue).launch(new BadDataException("Invalid Catalogue URL"));
+        Validate.isNull(catalogue).launch(new BadDataException(errorMessage));
         return this.objectMapper.readTree(catalogue);
-    }
-
-    @SneakyThrows
-    private JsonNode getServiceOffering(String serviceOfferingUrl) {
-        String serviceOffering = InvokeService.executeRequest(serviceOfferingUrl, HttpMethod.GET);
-        Validate.isNull(serviceOffering).launch(new BadDataException("Invalid Service Offering ID"));
-        return this.objectMapper.readValue(serviceOffering, JsonNode.class);
     }
 
     private Policy getPolicyForServiceOffer(String policyUrl) {
@@ -230,7 +223,7 @@ public class PolicyService {
     }
 
     public boolean evaluatePolicy(PolicyEvaluationRequest policyEvaluationRequest) {
-        JsonNode catalogueDescription = this.getCatalogueDescription(policyEvaluationRequest.catalogueUrl());
+        JsonNode catalogueDescription = this.getServiceOffering(policyEvaluationRequest.catalogueUrl(), "invalid.catalogue.url");
         List<String> catalogueSubdivisionCodeList;
 
         try {
@@ -243,7 +236,7 @@ public class PolicyService {
             throw new BadDataException("catalogue.location.invalid");
         }
 
-        JsonNode serviceOffer = this.getServiceOffering(policyEvaluationRequest.serviceOfferId());
+        JsonNode serviceOffer = this.getServiceOffering(policyEvaluationRequest.serviceOfferId(), "invalid.service.offer.url");
         JsonNode servicePolicyArray = this.getPolicyArrayFromServiceOffer(serviceOffer);
 
         if (servicePolicyArray != null && servicePolicyArray.has(0)) {
@@ -251,9 +244,7 @@ public class PolicyService {
                 String policyUrl = policyUrlJsonNode.asText();
                 if (policyUrl.endsWith(JSON_EXTENSION)) {
                     Constraint serviceOfferSpatialConstraint = this.getLocationConstraintFromPolicy(policyUrl);
-
-                    Constraint constraint = this.getLocationConstraintFromPolicy(policyUrl);
-                    return this.isCountryInPermittedRegion(countryCode, constraint);
+                    return this.isCountryInPermittedRegion(catalogueSubdivisionCodeList, serviceOfferSpatialConstraint);
                 }
             }
         }
